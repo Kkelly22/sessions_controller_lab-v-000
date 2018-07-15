ffc781f9137dfffe1707dc24cddbee4f552a7e1a
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 13:05:00 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe "sessions/new.html.erb", type: :view do
 
   describe 'session new' do
-    it 'shows a form to login' do
+    it 'user is able to login' do
       visit (sessions/new)
+      fill_in('name', :with => 'Kaitlin')
+      click_button('login')
       expect(response).to redirect_to controller: 'sessions', action: 'new'
     end
   end
 
-  
+
 end
