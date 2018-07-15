fe37669673e98ca4ca3a0198950271a0d93b3dcd
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 13:15:13 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe "sessions/new.html.erb", type: :view do
   describe 'session new' do
     it 'shows a login form' do
       session[:name] = "Kaitlin"
-      render
-      expect(rendered).to include 'Kaitlin'
+      find_button('login').click
     end
   end
 
