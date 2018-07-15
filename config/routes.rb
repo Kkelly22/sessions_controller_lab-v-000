b6911b817f30f8837c9e189e99155549cf115fae
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 12:47:37 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+      resources :session, only: [:index, :new, :create]
 
   # Example resource route with options:
   #   resources :products do
