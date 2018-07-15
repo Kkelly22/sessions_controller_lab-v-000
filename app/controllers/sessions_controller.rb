d541270e2fa1617d911d667087eb9912cd66967a
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 12:58:32 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete :name
+    if !!session[:name]
+      session.delete :name
+    end
     redirect_to '/sessions/new'
   end
 
